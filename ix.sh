598f9230c7755fa464420ce5c87251ee5295970f
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-01 00:39:33 +0800

    Update ix.sh

--- ix.sh
+++ ix.sh
@@ -270,7 +270,10 @@ removebeep || error
 # Remove unnecessary files and other cleaning
 rm -r ~/ix/ /home/$username/.config/dotfiles/ /home/$username/README.md &&
 sudo -u $username mv /home/$username/go /home/$username/dox/ &&
-sudo -u $username mkdir /home/$username/.config/gtk-2.0 || error
+sudo -u $username mkdir /home/$username/.config/gtk-2.0 &&
+sudo -u $username mkdir -p /home/$username/.config/mpd/playlists/ &&
+sudo -u $username systemctl --user enable mpd.service >/dev/null 2>&1 &&
+sudo -u $username systemctl --user start mpd.service >/dev/null 2>&1 || error
 
 # Change shell to zsh
 changeshell || error
